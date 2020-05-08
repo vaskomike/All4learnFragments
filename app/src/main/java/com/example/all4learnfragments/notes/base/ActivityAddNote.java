b4b7211210b1ce166d2cbb3021ae6a335402a85a
vaskomike <62155899+vaskomike@users.noreply.github.com>
2020-05-08 18:10:12 +0300

    finish fragments ? idk

---
+++ app/src/main/java/com/example/all4learnfragments/notes/base/ActivityAddNote.java
@@ -0,0 +1,75 @@
+package com.example.all4learnfragments.notes.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.all4learnfragments.R;
+import com.example.all4learnfragments.notes.Note;
+import com.example.all4learnfragments.notes.maper.NotesMapper;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class ActivityAddNote extends AppCompatActivity {
+
+
+    private FirebaseFirestore fireStore = FirebaseFirestore.getInstance();
+
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+
+    private static TextView dateNote;
+
+    private TextInputEditText titleInputEditText, textInputEditText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_note_activity);
+
+        titleInputEditText = findViewById(R.id.title);
+        textInputEditText = findViewById(R.id.text);
+        dateNote = findViewById(R.id.dateNote);
+        dateNote.setText(noteDate);
+
+    }
+
+    private String getUid() {
+        return firebaseAuth.getCurrentUser().getUid();
+    }
+
+    Calendar calendar = Calendar.getInstance();
+    static final SimpleDateFormat format = new SimpleDateFormat("dd-MM-yy HH:mm");
+
+    private String noteDate = format.format(calendar.getTime());
+
+    public Note saveNote(String title, String text, String date) {
+
+        title = titleInputEditText.getText().toString();
+        text = textInputEditText.getText().toString();
+
+        if (!title.equals("") || !text.equals("")) {
+            DocumentReference reference = fireStore.collection(NotesMapper.COLLECTION).document();
+            reference.set(NotesMapper.newNote(getUid(), title, text, noteDate));
+            return new Note(reference.getId(), getUid(), title, text, date);
+        } else Toast.makeText(ActivityAddNote.this, "Empty note", Toast.LENGTH_SHORT).show();
+        return null;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveNote(titleInputEditText.toString(), textInputEditText.toString(), noteDate);
+        setResult(Activity.RESULT_OK);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/all4learnfragments/notes/base/EditNoteActivity.java
@@ -0,0 +1,102 @@
+package com.example.all4learnfragments.notes.base;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.all4learnfragments.R;
+import com.example.all4learnfragments.notes.Note;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.all4learnfragments.notes.maper.NotesMapper.COLLECTION;
+import static com.example.all4learnfragments.notes.maper.NotesMapper.DATE;
+import static com.example.all4learnfragments.notes.maper.NotesMapper.TEXT;
+import static com.example.all4learnfragments.notes.maper.NotesMapper.TITLE;
+
+public class EditNoteActivity extends AppCompatActivity {
+
+    private static final String EXTRA_NOTE = "note";
+
+    private FirebaseFirestore fireStore = FirebaseFirestore.getInstance();
+
+    private TextView dateNote;
+
+    private TextInputEditText titleInputEditText, textInputEditText;
+
+    Calendar calendar = Calendar.getInstance();
+    static final SimpleDateFormat format = new SimpleDateFormat("dd-MM-yy HH:mm");
+    private String noteDate = format.format(calendar.getTime());
+
+    public static Intent createIntent(Context context, Note item) {
+        Intent intent = new Intent(context, EditNoteActivity.class);
+        intent.putExtra(EXTRA_NOTE, item);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_note_activity);
+
+        titleInputEditText = findViewById(R.id.title);
+        textInputEditText = findViewById(R.id.text);
+        dateNote = findViewById(R.id.dateNote);
+
+        Note note = getIntent().getParcelableExtra(EXTRA_NOTE);
+        if (note == null) {
+            finish();
+            return;
+        }
+        titleInputEditText.setText(note.getTitle());
+        textInputEditText.setText(note.getText());
+        dateNote.setText(note.getDate());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Note note = getIntent().getParcelableExtra(EXTRA_NOTE);
+        if (note == null) {
+            finish();
+            return;
+        }
+
+        DocumentReference edit = fireStore.collection(COLLECTION).document(note.getId());
+        Map<String, Object> data = new HashMap<>();
+
+        if (!note.getTitle().equals(titleInputEditText.getText().toString())) {
+            data.put(TITLE, titleInputEditText.getText().toString());
+        }
+
+        if (!note.getText().equals(textInputEditText.getText().toString())) {
+            data.put(TEXT, textInputEditText.getText().toString());
+        }
+        data.put(DATE, dateNote);
+
+        if (note.getTitle().equals(titleInputEditText.getText().toString()) && note.getText().equals(textInputEditText.getText().toString())) {
+            data.put(DATE, note.getDate());
+        }
+
+        data.put(DATE, noteDate);
+        edit.set(
+                data,
+                SetOptions.merge()
+        );
+        setResult(Activity.RESULT_OK);
+        finish();
+    }
+
+}
