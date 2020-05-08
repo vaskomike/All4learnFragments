afdc6c9a5088a51207869a0467f9c77b00a59396
vaskomike <62155899+vaskomike@users.noreply.github.com>
2020-05-08 17:56:06 +0300

    finish fragments ? idk

--- app/src/main/java/com/example/all4learnfragments/notes/NotesFragment.java
+++ app/src/main/java/com/example/all4learnfragments/notes/NotesFragment.java
@@ -16,6 +16,7 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.example.all4learnfragments.R;
 import com.example.all4learnfragments.notes.base.DeleteNote;
+import com.example.all4learnfragments.notes.base.EditNoteActivity;
 import com.example.all4learnfragments.notes.base.NotesAdapter;
 import com.example.all4learnfragments.notes.base.NotesPresenter;
 import com.example.all4learnfragments.notes.utils.OnItemClickListener;
@@ -27,6 +28,7 @@ public class NotesFragment extends Fragment implements NotesPresenter.Listener, 
     private NotesAdapter adapter;
 
     NotesPresenter loadNotes = new NotesPresenter(this);
+
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
@@ -38,18 +40,20 @@ public class NotesFragment extends Fragment implements NotesPresenter.Listener, 
         return inflater.inflate(R.layout.notes_fragment, container, false);
 
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         adapter = new NotesAdapter(getLayoutInflater(), this);
 
 
         super.onViewCreated(view, savedInstanceState);
-        RecyclerView recyclerView =(RecyclerView) view.findViewById(R.id.recyclerNotes);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerNotes);
         recyclerView.setAdapter(adapter);
         new ItemTouchHelper(callback).attachToRecyclerView(recyclerView);
         StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
     }
+
     ItemTouchHelper.SimpleCallback callback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
         @Override
         public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
@@ -70,6 +74,7 @@ public class NotesFragment extends Fragment implements NotesPresenter.Listener, 
 
     @Override
     public void onItemClicked(Note item) {
+        startActivity(EditNoteActivity.createIntent(getActivity(), item));
 
     }
 }
